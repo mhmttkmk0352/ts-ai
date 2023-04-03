7e66fdcdbb5bbf35e78fe881bb88adbd51ff2d74
mehmet.tokmak <mehmet.tokmak@beyn.com.tr>
2023-04-04 02:47:19 +0300

    .

--- www-2/js/canvas.js
+++ www-2/js/canvas.js
@@ -71,7 +71,7 @@ window.onload = function () {
     whenLoad();
     context.clearRect(0, 0, canvas.width, canvas.height);
 
-    draw(300, 300, 10000);
+    draw(300, 300, 1000);
 
     moveCell();
 
